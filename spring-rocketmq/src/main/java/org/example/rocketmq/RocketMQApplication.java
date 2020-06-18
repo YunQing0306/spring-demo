23c7b62b49e6c4e088970f529fdf2adf4a424cce
YunQing <YunQing0306@gmail.com>
2020-06-18 22:27:30 +0800

    change gitignore

--- spring-rocketmq/src/main/java/org/example/rocketmq/RocketMQApplication.java
+++ spring-rocketmq/src/main/java/org/example/rocketmq/RocketMQApplication.java
@@ -1,9 +1,12 @@
 package org.example.rocketmq;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RocketMQApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMQApplication.class,args);
+    }
 
 }
